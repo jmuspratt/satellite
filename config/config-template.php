0894ec054b515d33a1484732ca2ad5ae6323b42a
James Muspratt <jmuspratt@gmail.com>
2013-06-08 09:35:49 -0400

    date taken/uploaded is now optional via config

--- config/config-template.php
+++ config/config-template.php
@@ -1,14 +1,18 @@
 <?php
 $config = array(
+	
+	
+	// -------------------- Required Info --------------------
 
-	// Give your gallery a title. Internal Double quotation marks ("") should be escaped or replaced by HTML entities.
-	// Example: "Joe's \"Quality\" Photographs". 
+	// Give your gallery a title. Internal Double quotation marks ("") should be escaped as \" or replaced by HTML entities. See http://danshort.com/HTMLentities/
+	// Example: "Joe&#8217;s \"Quality\" Photographs".
 	"gallery_title" => "",
 
-	// Insert your API key
+	// Insert your API key, copied from http://www.flickr.com/services/apps/create/apply (choose non-commercial)
+	// Example 
 	"key" => "",
 
-	// Insert your Flickr username. This is not your Yahoo sign in username, or your flickr screename - but the username you use for Flickr itself. To double check, sign in to Flickr and look at the top of the page where it says 'Signed in as...' - that is your username.
+	// Insert your Flickr username. This is not your Yahoo sign in username, or your Flickr screename - but the username you use for Flickr itself. To double check, sign in to Flickr and look at the top of the page where it says 'Signed in as...'. that is your username.
 	// Example: "joeschmoe"
 	"username"=> "",
 		
@@ -16,9 +20,20 @@ $config = array(
 	// Example: "http://mydomain.com/photos/"
 	"root_url" => "",
 	
+
+	// -------------------- Thumbnail Pages --------------------
+	
 	// On thumbnail views, how many photos per page? No quotation marks around numbers.
-	// Example: 2
-	"photos_per_page" => 18,
+	// Example: 24
+	"photos_per_page" => 24,
+	
+	
+	// -------------------- Single Image/Video Page --------------------
+	
+	// Show photo taken and upload dates? No quotation marks around booleans.
+	// Example: true
+	"show_date_taken" => true,
+	"show_date_uploaded" => false
 	
 	);
 
