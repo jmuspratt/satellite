0b0f74551660add19a776a9bb7d4e924a8462c99
James Muspratt <jmuspratt@gmail.com>
2013-06-11 09:41:56 -0400

    move js to footer

--- inc/doc-head.php
+++ inc/doc-head.php
@@ -21,12 +21,6 @@
 		<script src="/js/respond.min.js"></script>
 	<![endif]-->
 		
-		<!-- jQuery  -->
-		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
-		<script>window.jQuery || document.write("<script src='<?php echo $config["root_url"]; ?>/js/jquery-1.10.1.min.js'>\x3C/script>")</script>
-		<!-- scripts -->
-		<script src="<?php echo $config["root_url"]; ?>/js/respond.min.js"></script>
-		<script src="<?php echo $config["root_url"]; ?>/js/jquery.cookie.js"></script>
-		<script src="<?php echo $config["root_url"]; ?>/js/scripts.js"></script>
+
 
 </head>
--- inc/footer.php
+++ inc/footer.php
@@ -10,3 +10,13 @@
 	</p>
 
 </footer> <!-- footer -->
+
+
+<!-- jQuery  -->
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+<script>window.jQuery || document.write("<script src='<?php echo $config["root_url"]; ?>/js/jquery-1.10.1.min.js'>\x3C/script>")</script>
+<!-- scripts -->
+<script src="<?php echo $config["root_url"]; ?>/js/respond.min.js"></script>
+<script src="<?php echo $config["root_url"]; ?>/js/jquery.cookie.js"></script>
+<script src="<?php echo $config["root_url"]; ?>/js/picturefill.js"></script>
+<script src="<?php echo $config["root_url"]; ?>/js/scripts.js"></script>
