13bc95226450eb52112ae4d5ec2816541a4cbd56
James Muspratt <jmuspratt@gmail.com>
2013-06-11 12:03:02 -0400

    trailing slash on sets

--- inc/header.php
+++ inc/header.php
@@ -4,7 +4,7 @@
 			<h1><a href="<?php echo $root_url;?>"><?php echo $config["gallery_title"]; ?></a></h1>
 			<ul>
 				<li<?php if ($this_page == "home") { ?> class="current" <?php } ?>><a href="<?php echo $root_url;?>">Home</a></li>
-				<li<?php if ($this_page == "set-view" || $this_page == "sets") { ?> class="current" <?php } ?>><a href="<?php echo $root_url;?>sets">Sets</a></li>
+				<li<?php if ($this_page == "set-view" || $this_page == "sets") { ?> class="current" <?php } ?>><a href="<?php echo $root_url;?>sets/">Sets</a></li>
 			</ul>
 		</nav>
 	
