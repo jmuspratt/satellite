0ad9cf1a701911f285f569725a3ae075b4a99b74
James Muspratt <jmuspratt@gmail.com>
2013-06-17 16:57:15 -0400

    add border to navigation elements

--- inc/header.php
+++ inc/header.php
@@ -3,9 +3,7 @@
 		<nav class="primary">
 			<h1><a href="<?php echo $root_url;?>"><?php echo $config["gallery_title"]; ?></a></h1>
 			<ul>
-				<li<?php if ($this_page == "home") { ?> class="current" <?php } ?>><a href="<?php echo $root_url;?>">Home</a></li>
-				<li<?php if ($this_page == "set-view" || $this_page == "sets") { ?> class="current" <?php } ?>><a href="<?php echo $root_url;?>sets/">Sets</a></li>
-				<li<?php if ($this_page == "tag-view" || $this_page == "tags") { ?> class="current" <?php } ?>><a href="<?php echo $root_url;?>tags/">Tags</a></li>
+				<li<?php if ($this_page == "home") { ?> class="current" <?php } ?>><a href="<?php echo $root_url;?>">Home</a></li><li<?php if ($this_page == "set-view" || $this_page == "sets") { ?> class="current" <?php } ?>><a href="<?php echo $root_url;?>sets/">Sets</a></li><li<?php if ($this_page == "tag-view" || $this_page == "tags") { ?> class="current" <?php } ?>><a href="<?php echo $root_url;?>tags/">Tags</a></li>
 			</ul>
 		</nav>
 	
